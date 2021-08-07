997a8401fdab7482404f704f6dec1a60ad3b4013
sandeeppujare <sp0068@gmail.com>
2021-08-07 16:33:53 +0530

    Done dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import produce from 'immer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const numRows = 33;
+const numCols = 72;
+
+const operations = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, ,0],
+    [-1, 0]
+];
+
+const App = () => {
+
+    const [grid, setGrid] = useState(() => {
+        const rows = [];
+
+        for (let i = 0; i < numRows; i++) {
+             rows.push(Array.from(Array(numCols), () => 0));
+        }
+
+        return rows;
+    });
+
+    const [running, setRunning] = useState(false);
+
+    const runningRef = useRef(running);
+    runningRef.current = running;
+
+    const runSimulation = useCallback(() => {
+        if ( !runningRef.current ) {
+            return;
+        }
+        
+        setGrid(g => {
+            return produce(g, gridCopy => {
+                for ( let i = 0; i < numRows; i++ ) {
+                    for ( let j = 0; j < numCols; j++ ) {
+                        let neighbours = 0;
+
+                        operations.forEach(([x, y]) => {
+                            const newI = i + x;
+                            const newJ = j + y;
+
+                            if( newI >= 0 && newI < numRows && newJ >=0 && newJ <numCols ) {
+                                neighbours += g[newI][newJ];
+                            }
+                        });
+
+                        if( neighbours < 2 || neighbours > 3 ) {
+                            gridCopy[i][j] = 0;
+                        } else if ( g[i][j] === 0 && neighbours === 3) {
+                            gridCopy[i][j] = 1;
+                        }
+                    }
+                }
+            })
+        })
+
+        setTimeout(runSimulation, 1000);
+    }, [])
+
+    return (
+            <>
+                <button
+                    onClick = {
+                        () => {
+                             setRunning(!running);
+                             runningRef.current = true;
+                             runSimulation();
+                        }
+                    }
+                    >
+                    {running ? 'Stop' : 'Start'}
+                </button>
+                <div 
+                    style={{
+                        display: 'grid', 
+                        gridTemplateColumns: `repeat(${numCols}, 20px)`
+                    }}>
+
+                    {
+                        grid.map((rows, i) => rows.map((col, k) => <div 
+                            key={`${i}-${k}`} 
+                            onClick = {() => {
+                                const newGrid = produce(grid, gridCopy => {
+                                    gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                                })
+                                setGrid(newGrid);
+                            }}
+                            style= {{  
+                                width : 20, 
+                                height : 20, 
+                                backgroundColor : grid[i][k] ? 'pink' : undefined, 
+                                border : 'solid 1px black'
+                    }}/>))}
+                </div>
+            </>
+        )
 }
 
 export default App;
